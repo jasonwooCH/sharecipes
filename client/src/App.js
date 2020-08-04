dbc7bb3b7344ab977979e1aba074f4f0a716649d
🤔Chul Hee Woo <wochul@amazon.com>
2020-08-03 22:32:08 -0700

    Redux store initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
